2fe33b8bb6978892bae4655f103f77201410c409
santrepo <santhoshmurugezan@outlook.com>
2025-12-01 18:12:55 +0530

    commit from demobranch for conflict test

--- switchcase.sh
+++ switchcase.sh
@@ -27,3 +27,30 @@ case variable in
   pattern2) commands ;;
   *) default ;;
 esac
+
+
+#!/bin/bash
+
+read -p "Enter a number (1-7): " day
+
+case $day in
+  1) echo "Sunday" ;;
+  2) echo "Monday" ;;
+  3) echo "Tuesday" ;;
+  4) echo "Wednesday" ;;
+  5) echo "Thursday" ;;
+  6) echo "Friday" ;;
+  7) echo "Saturday" ;;
+  *) echo "Invalid number" ;;
+esac
+
+
+#!/bin/bash
+
+read -p "Enter a character: " ch
+
+case $ch in
+  [aeiouAEIOU]) echo "Vowel" ;;
+  [a-zA-Z]) echo "Consonant" ;;
+  *) echo "Invalid input" ;;
+esac
